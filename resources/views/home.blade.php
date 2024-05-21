f33fceab50f270def48158abef314505d60cfd8b
Nik0-9 <filannino.nicola9@gmail.com>
2024-05-21 20:01:32 +0200

    add box in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,19 @@
 @section('title', 'Home')
 
 @section('content')
-<main>
-    <h1>Home page</h1>
+<main class="container vh-100">
 
+<div class="container-fluid h-100 d-flex justify-content-center align-items-center">
+    <div class="d-flex justify-content-around w-100">
+      <div class="mybox">
+        <a href="{{route('books.index')}}"><h2>Books</h2></a>
+      </div>
+      <div class="mybox">
+      <a href="{{route('movies.index')}}"><h2>Movies</h2></a>
+
+      </div>
+    </div>
+  </div>
 </main>
 
 @endsection
